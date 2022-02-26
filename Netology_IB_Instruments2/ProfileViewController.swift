dd102bacd24e9a32cc2a95afcd76637a9fa84e28
JuliaKornisina <julia20m@mail.ru>
2022-02-26 20:57:02 +0300

    Add Profile.storyboard ProfileViewController

---
+++ Netology_IB_Instruments2/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments2
+//
+//  Created by Юлия Малюгина on 26.02.2022.
+//
+
+import Foundation
+
+class ProfileViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
